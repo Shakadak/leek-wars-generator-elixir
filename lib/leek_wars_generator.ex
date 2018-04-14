0ccc3f602b2effd37e5c3e8066e5b6503cad9180
shakadak <nathanael.pineau@gmail.com>
2018-04-14 10:26:53 +0200

    Initial commit

---
+++ lib/leek_wars_generator.ex
@@ -0,0 +1,18 @@
+defmodule LeekWarsFightGenerator do
+  @moduledoc """
+  Documentation for LeekWarsFightGenerator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LeekWarsFightGenerator.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/leek_wars_generator_test.exs
@@ -0,0 +1,8 @@
+defmodule LeekWarsFightGeneratorTest do
+  use ExUnit.Case
+  doctest LeekWarsFightGenerator
+
+  test "greets the world" do
+    assert LeekWarsFightGenerator.hello() == :world
+  end
+end
